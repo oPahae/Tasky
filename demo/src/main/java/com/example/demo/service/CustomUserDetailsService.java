b3b732a1f6a3ca71a029a0bffbcaeaef1d16378c
Tayef Jihane <tayefjihane1@gmail.com>
2025-11-28 22:56:07 +0100

    auth

--- demo/src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ demo/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.repository.UserRepository;
+import com.example.demo.repositories.UserRepository;
 
 
     @Service
