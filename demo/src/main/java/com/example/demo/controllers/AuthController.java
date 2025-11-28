dadfc47888f27d9b487df8c59ac05c644fc5d155
Tayef Jihane <tayefjihane1@gmail.com>
2025-11-28 20:09:50 +0100

    auhentification

---
+++ demo/src/main/java/com/example/demo/controllers/AuthController.java
@@ -0,0 +1,70 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.jwt.JwtUtil;
+import com.example.demo.repository.UserRepository;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired 
+    private UserRepository repo;
+
+    @Autowired 
+    private PasswordEncoder encoder;
+
+    @Autowired 
+    private JwtUtil jwtUtil;
+
+    @Autowired 
+    private AuthenticationManager authenticationManager;
+
+    // REGISTER 
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest req) {
+
+        if (repo.existsByEmail(req.email())) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
+
+        User user = new User();
+        user.setEmail(req.email());
+        user.setPassword(encoder.encode(req.password()));
+        user.setRole("USER");
+
+        repo.save(user);
+
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    // LOGIN
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest req) {
+
+        // Authentification via email
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                req.email(), 
+                req.password()
+        );
+
+        authenticationManager.authenticate(auth);
+
+        // Génération du token en utilisant l'email
+        String token = jwtUtil.generateToken(req.email());
+
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+}
---
+++ demo/src/main/java/com/example/demo/jwt/JwtFilter.java
@@ -0,0 +1,58 @@
+package com.example.demo.jwt;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.demo.service.CustomUserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter { 
+
+    @Autowired
+    private JwtUtil jwtUtil; // bach naccediw lmethods dyal jwtUtil
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;// bach naccediw ldetails dyal user
+
+    // hadi method li katintercepti kol request u katchecki wach fih token
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");// kanjibo lheader dyal Authorization mn request
+
+        //ila kan token kan9rawou u kanextractiw username mnou
+        //sinon kankheliwh iduz
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);// kanextractiw token mn header
+            String username = jwtUtil.extractUsername(token);// kanextractiw username mn token
+            
+            //kanjibo details dyal user mn database
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        //nkheliw passer request vers controller
+        filterChain.doFilter(request, response);
+    }
+}
+
+//cette classe JwtFilter est un filtre de sécurité qui intercepte chaque requête HTTP entrante. 
+//verifie si le client a envoyé un token JWT 
+//si oui, elle extrait le nom d'utilisateur du token, charge les détails de l'utilisateur correspondant,
