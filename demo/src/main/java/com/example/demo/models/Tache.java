ca7a217afa504373c7c88e62de61f79bfd706bfb
pahae-tests <wahipahae@gmail.com>
2025-11-28 18:30:54 +0100

    test rapide

--- demo/src/main/java/com/example/demo/models/Tache.java
+++ demo/src/main/java/com/example/demo/models/Tache.java
@@ -5,10 +5,10 @@ import java.util.Date;
 import java.util.List;
 
 public class Tache {
-
-    private int id;
-    private String titre;
-    private String description;
+    
+    private  int id;
+    private  String titre;
+    private  String description;
     private LocalDateTime dateCreation;
     private LocalDateTime dateFin;
     private LocalDateTime dateLimite;
@@ -16,12 +16,12 @@ public class Tache {
     private String etat;
     private Projet projet;
     private Membre membre;
-    private List<Commentaire> commentaires;
-    private List<Blockage> blockage;
+    private List <Commentaire> commentaires;
+    private List <Blockage> blockage;
     private List<Document> documents;
 
-    public Tache(String titre, String description, LocalDateTime dateLimite, List<SousTache> sousTaches, Projet projet, Membre membre) {
-
+ public Tache( String titre, String description, LocalDateTime dateLimite, List<SousTache> sousTaches, Projet projet, Membre membre) {
+       
         this.titre = titre;
         this.description = description;
         this.dateLimite = dateLimite;
@@ -29,117 +29,94 @@ public class Tache {
         this.etat = "en cours";
         this.projet = projet;
         this.membre = membre;
-        this.dateCreation = LocalDateTime.now();
+        this.dateCreation=LocalDateTime.now();
     }
 
     public int getId() {
-        return id;
-    }
+        return id; }
 
     public void setId(int id) {
-        this.id = id;
-    }
+        this.id = id;}
 
     public String getTitre() {
-        return titre;
-    }
+        return titre; }
 
     public void setTitre(String titre) {
-        this.titre = titre;
-    }
+        this.titre = titre;}
 
     public String getDescription() {
-        return description;
-    }
+        return description;}
 
     public void setDescription(String description) {
-        this.description = description;
-    }
+        this.description = description;}
 
     public LocalDateTime getDateCreation() {
-        return dateCreation;
-    }
+        return dateCreation;}
 
     public void setDateCreation(LocalDateTime dateCreation) {
-        this.dateCreation = dateCreation;
-    }
+        this.dateCreation = dateCreation; }
 
     public LocalDateTime getDateFinale() {
-        return dateFin;
-    }
+        return dateFin; }
 
     public void setDateFinale(LocalDateTime dateFinale) {
-        this.dateFin = dateFinale;
-    }
+        this.dateFin = dateFinale;}
 
     public List<SousTache> getSousTaches() {
-        return sousTaches;
-    }
+        return sousTaches; }
 
     public void setSousTaches(List<SousTache> sousTaches) {
-        this.sousTaches = sousTaches;
-    }
+        this.sousTaches = sousTaches; }
 
     public String getEtat() {
-        return etat;
-    }
+        return etat; }
 
     public void setEtat(String etat) {
         this.etat = etat;
-        if (etat.equals("terminée")) {
-            this.dateFin = LocalDateTime.now();
-        }
-    }
-
+        if(etat.equals("terminée")) {
+               this.dateFin=LocalDateTime.now();
+            } }
+    
     public Projet getProjet() {
-        return projet;
-    }
+        return projet;}
 
     public void setProjet(Projet projet) {
-        this.projet = projet;
-    }
+        this.projet = projet; }
 
     public Membre getMembre() {
-        return membre;
-    }
+        return membre; }
 
     public void setMembre(Membre membre) {
         this.membre = membre;
     }
-
     public List<Commentaire> getCommentaires() {
         return commentaires;
     }
-
     public void setCommentaires(List<Commentaire> commentaires) {
         this.commentaires = commentaires;
     }
-
     public List<Blockage> getBlockage() {
         return blockage;
     }
-
     public void setBlockage(List<Blockage> blockage) {
         this.blockage = blockage;
     }
-
     public List<Document> getDocuments() {
         return documents;
     }
-
     public void setDocuments(List<Document> documents) {
         this.documents = documents;
     }
-
     public LocalDateTime getDateLimite() {
-        return dateLimite;
+        return dateLimite ;
     }
-
-    public void setDateLimite(LocalDateTime dateLimite) {
+    public void setDateLimite(LocalDateTime  dateLimite) {
         this.dateLimite = dateLimite;
-        if (this.etat.equals("en cours") && dateLimite.isBefore(LocalDateTime.now())) {
-            etat = "Pas fini";
-        }
-
+        if(this.etat.equals("en cours") && dateLimite.isBefore(LocalDateTime.now())) {
+            etat="Pas fini";
     }
+
+
+
+}
 }
