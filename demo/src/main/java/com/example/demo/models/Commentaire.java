b4559143493f19f4ad9b9061544385d2c39995cb
Tayef Jihane <tayefjihane1@gmail.com>
2025-11-28 23:09:51 +0100

    authv2

--- demo/src/main/java/com/example/demo/models/Commentaire.java
+++ demo/src/main/java/com/example/demo/models/Commentaire.java
@@ -1,8 +1,15 @@
 package com.example.demo.models;
 
-import jakarta.persistence.*;
 
 import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Commentaire")
