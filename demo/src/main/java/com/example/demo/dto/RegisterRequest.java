d93404e9e08dea29a4c313da0f00f6864781fec2
pahae-tests <wahipahae@gmail.com>
2025-11-29 12:13:59 +0100

    cc

--- demo/src/main/java/com/example/demo/dto/RegisterRequest.java
+++
@@ -1,8 +0,0 @@
-// package com.example.demo.dto;
-
-
-//     public record RegisterRequest(String email, String password) {}
-
-// /* record katsawb automatiquement les getters, constructors u toString u kikuno fiha que les donnes li 3titiha 
-//  RegisterRequest hadi classe li ghadi tsta3ml bach nreceviw email u password mlli user ydir register
-//  */
