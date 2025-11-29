db31bcf302fb72d1d6bf62265223ba011595beca
pahae <151317006+oPahae@users.noreply.github.com>
2025-11-29 20:16:56 +0100

    Update MesProjetsController.java

--- demo/src/main/java/com/example/demo/controllers/MesProjetsController.java
+++ demo/src/main/java/com/example/demo/controllers/MesProjetsController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controllers;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.Projet;
@@ -14,119 +14,159 @@ import com.example.demo.hooks.MembreDTO;
 import com.example.demo.hooks.ProjetDTO;
 import com.example.demo.models.Membre;
 import com.example.demo.repositories.ProjetRepository;
+import com.example.demo.repositories.MembreRepository;
+
+
+
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/api")
 public class MesProjetsController {
     @Autowired
     private ProjetRepository projetRepository;
+     @Autowired
+    private MembreRepository membreRepository;
 
-    @GetMapping("Projet/all")
-    public List<ProjetDTO> getAllProjet() {
-        return projetRepository.findAll().stream()
+    @GetMapping("projet/all")
+    public List<ProjetDTO> getAllProjet(){
+           return projetRepository.findAll().stream()
                 .map(this::convertToProjetDTO)
                 .collect(Collectors.toList());
     }
-
-    @DeleteMapping("Projet/deleteAll")
-    public void deleteAllProject() {
+    @DeleteMapping("projet/deleteAll")
+    public void deleteAllProject(){
         projetRepository.deleteAll();
     }
-
-    @DeleteMapping("Projet/delete/{id}")
-    public void deleteProjet(int id) {
+    @DeleteMapping("projet/delete/{id}")
+     public void deleteProjet(@PathVariable int id){
         projetRepository.deleteById(id);
-    }
-
-    @PostMapping("Projet/add")
-    public void addProjet(Projet p) {
+     }
+     @PostMapping("projet/add")
+     public void addProjet(Projet p){
         projetRepository.save(p);
-    }
-
-    @GetMapping("/Projet/membre/{id}")
-    public List<ProjetDTO> projetByIdMembre(@PathVariable int id) {
-        return projetRepository.findByMembres_Id(id).stream()
+     }
+     @GetMapping("/projet/membre/{id}")
+     public List<ProjetDTO> projetByIdMembre(@PathVariable int id) {
+    return projetRepository.findByMembres_Id(id).stream()
                 .map(this::convertToProjetDTO)
                 .collect(Collectors.toList());
+        }
+    
+    @PutMapping("projet/modifier/{id}")
+    public boolean updateProjetById(@PathVariable int id, Projet pr){
+                    Projet pr1 = projetRepository.findById(id);
+                    if(pr1 != null){
+                        pr1.setNom(pr.getNom());
+                        pr1.setDescription(pr.getDescription());
+                        pr1.setBudget(pr.getBudget());
+                        pr1.setBudgetConsomme(pr.getBudgetConsomme());
+                        pr1.setCode(pr.getCode());
+                        pr1.setDeadline(pr.getDeadline());
+                        pr1.setDateFin(pr.getDateFin());
+                        projetRepository.save(pr1);
+                        return true;
+                    }
+                    return false;
     }
 
-    @PutMapping("Projet/modifier/{id}")
-    public boolean updateProjetById(@PathVariable int id, Projet pr) {
-        Projet pr1 = projetRepository.findById(id);
-        if (pr1 != null) {
-            pr1.setNom(pr.getNom());
-            pr1.setDescription(pr.getDescription());
-            pr1.setBudget(pr.getBudget());
-            pr1.setBudgetConsomme(pr.getBudgetConsomme());
-            pr1.setCode(pr.getCode());
-            pr1.setDeadline(pr.getDeadline());
-            pr1.setDateFin(pr.getDateFin());
-            projetRepository.save(pr1);
+
+
+
+      @GetMapping("projet/{id}/Membre")
+    public List<MembreDTO> getMembreProjet(int id){
+        Projet pr=projetRepository.findById(id);
+        List<Membre>m= pr.getMembres();
+        if(pr!=null && m!=null){
+               return m.stream().map(this::convertToMembreDTO).collect(Collectors.toList());
+        }
+        return null;
+     
+    }
+ private ProjetDTO convertToProjetDTO(Projet t) {
+        if (t == null) return null;
+        ProjetDTO dto = new ProjetDTO();
+        dto.id=t.getId();
+        dto.nom=(t.getNom());
+        dto.code=(t.getCode());
+        dto.description=(t.getDescription());
+        dto.dateDebut=(t.getDateDebut());
+        dto.dateFin=(t.getDateFin());
+        dto.deadline=(t.getDeadline());
+        dto.budget=(t.getBudget());
+        dto.budgetConsomme=(t.getBudgetConsomme());
+
+
+        return dto;}
+
+
+    private MembreDTO convertToMembreDTO(Membre t) {
+        if (t == null) return null;
+        MembreDTO dto = new MembreDTO();
+        dto.id=t.getId();
+        dto.nom=(t.getNom());
+        dto.email=(t.getEmail());
+        dto.description=(t.getDescription());
+        dto.password=(t.getPassword());
+        dto.role=(t.getRole());
+        dto.type=(t.getType());
+
+
+        return dto;}
+    @GetMapping("/Projet/{id}/progress")
+    public double getProgressProjet(@PathVariable int id){
+        Projet p=projetRepository.findById(id);
+        if(p!=null){
+           List <Tache> taches=p.getTaches();
+           if(taches.size()==0){
+            return 0;
+           }
+              double total=0;
+              total=taches.stream()
+                .filter(t -> "terminé".equalsIgnoreCase(t.getEtat()))
+                .count();
+              return (total/taches.size())*100;
+        }
+        return 0;
+    }
+
+    @GetMapping("projet/{idProjet}/addmembre/{idMembre}")
+    public boolean rejoindreAuProjet(@PathVariable("idProjet")int id,@PathVariable("idMembre")int idm){
+        Projet p=projetRepository.findById(id);
+        Membre m=membreRepository.findById(idm).orElse(null);
+        if(p!=null){
+            p.getMembres().add(m);
+            projetRepository.save(p);
             return true;
         }
         return false;
     }
 
-    @GetMapping("dashboard/projet/{id}/Membre")
-    public List<MembreDTO> getMembreProjet(int id) {
-        Projet pr = projetRepository.findById(id);
-        List<Membre> m = pr.getMembres();
-        if (pr != null && m != null) {
-            return m.stream().map(this::convertToMembreDTO).collect(Collectors.toList());
-        }
-        return null;
+    @PostMapping("projet/creer")
+    public ProjetDTO createProjet(@RequestBody ProjetDTO dto) {
+    Projet p = new Projet();
+    p.setNom(dto.nom);
+    p.setCode(dto.code);
+    p.setDescription(dto.description);
+    p.setBudget(dto.budget);
+    p.setBudgetConsomme(dto.budgetConsomme);
+    p.setDeadline(dto.deadline);
+    p.setDateDebut(dto.dateDebut);
+    p.setDateFin(dto.dateFin);
+    projetRepository.save(p);
 
-    }
+    return convertToProjetDTO(p); 
+}
 
-    private ProjetDTO convertToProjetDTO(Projet t) {
-        if (t == null)
-            return null;
-        ProjetDTO dto = new ProjetDTO();
-        dto.id = t.getId();
-        dto.nom = (t.getNom());
-        dto.code = (t.getCode());
-        dto.description = (t.getDescription());
-        dto.dateDebut = (t.getDateDebut());
-        dto.dateFin = (t.getDateFin());
-        dto.deadline = (t.getDeadline());
-        dto.budget = (t.getBudget());
-        dto.budgetConsomme = (t.getBudgetConsomme());
 
-        return dto;
-    }
 
-    private MembreDTO convertToMembreDTO(Membre t) {
-        if (t == null)
-            return null;
-        MembreDTO dto = new MembreDTO();
-        dto.id = t.getId();
-        dto.nom = (t.getNom());
-        dto.email = (t.getEmail());
-        dto.description = (t.getDescription());
-        dto.password = (t.getPassword());
-        dto.role = (t.getRole());
-        dto.type = (t.getType());
+ 
 
-        return dto;
-    }
 
-    @GetMapping("/Projet/{id}/progress")
-    public double getProgressProjet(@PathVariable int id) {
-        Projet p = projetRepository.findById(id);
-        if (p != null) {
-            List<Tache> taches = p.getTaches();
-            if (taches.size() == 0) {
-                return 0;
-            }
-            double total = 0;
-            total = taches.stream()
-                    .filter(t -> "terminé".equalsIgnoreCase(t.getEtat()))
-                    .count();
-            return (total / taches.size()) * 100;
-        }
-        return 0;
-    }
+    
+
+    
 }
