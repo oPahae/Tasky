be4d45870f8d2e5a0b5855ab57b2acca4fad4048
kafi-amina <aminakf35@gmail.com>
2025-12-01 02:32:22 +0100

    messagecontroller.java mais mazal mabghach ytcompila lia commd dyal maven

--- demo/src/main/java/com/example/demo/hooks/MessageDTO.java
+++ demo/src/main/java/com/example/demo/hooks/MessageDTO.java
@@ -9,12 +9,17 @@ public class MessageDTO {
      public String contenu;
      public Date dateEnvoi;
      public boolean estLu;
+     public int projetId; //zedt projetId = l’ID du projet auquel le message appartient
+     public int membreId; //zedt membreId = l’ID de l’utilisateur qui envoie le message
 
-     public MessageDTO(int id,String contenu ,Date dateE,boolean estLu) {
+
+     public MessageDTO(int id,String contenu ,Date dateE,boolean estLu,int projetId, int membreId) {
         this.contenu = contenu;
         this.id=id;
         this.dateEnvoi = dateE;
         this.estLu = estLu;
+        this.projetId = projetId;  //zedt
+        this.membreId = membreId;  //zedt
     }
-
+     public MessageDTO() {} // darori pour Spring
 }
